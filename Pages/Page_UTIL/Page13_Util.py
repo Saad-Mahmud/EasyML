baa4dcf6984359f56f2dccff4e613156160f7e02
saad <saadmahmud14@gmail.com>
2018-04-24 13:37:15 +0600

    Moved Page Graphics

--- Pages/Page_UTIL/Page13_Util.py
+++ Pages/Page_UTIL/Page13_Util.py
@@ -6,7 +6,7 @@ import dash_html_components as html
 import dash_table_experiments as dt
 from EasyML_Init import EM_App
 from Pages import EasyML_Page13
-from Pages import Page13_AB_Graphic
+from Pages.Page_Graphics import Page13_AB_Graphic
 
 
 class putil():
